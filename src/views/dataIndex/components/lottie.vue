4bf4c3875e0396aabb6979d308a50ab5a70a8142
15176371375@163.com <15176371375@163.com>
2019-06-21 15:58:39 +0800

    lottie

--- src/views/dataIndex/components/lottie.vue
+++ src/views/dataIndex/components/lottie.vue
@@ -1,42 +1,38 @@
 <template>
     <div :style="style" ref="lavContainer"></div>
 </template>
-
 <script>
-  import lottie from 'lottie-web';
-
-  export default {
-    props: {
-      options: {
-        type: Object,
-        required: true
-      },
-      height: Number,
-      width: Number,
+import lottie from 'lottie-web';
+export default {
+  props: {
+    options: {
+      type: Object,
+      required: true
     },
-
-    data () {
-      return {
-        style: {
-          width: this.width ? `${this.width}px` : '100%',
-          height: this.height ? `${this.height}px` : '100%',
-          overflow: 'hidden',
-          margin: '0 auto'
-        }
+    height: Number,
+    width: Number,
+  },
+  data () {
+    return {
+      style: {
+        width: this.width ? `${this.width}px` : '100%',
+        height: this.height ? `${this.height}px` : '100%',
+        overflow: 'hidden',
+        margin: '0 auto'
       }
-    },
-
-    mounted () {
-      this.anim = lottie.loadAnimation({
-          container: this.$refs.lavContainer,
-          renderer: 'svg',
-          loop: this.options.loop !== false,
-          autoplay: this.options.autoplay !== false,
-          animationData: this.options.animationData,
-          rendererSettings: this.options.rendererSettings
-        }
-      );
-      this.$emit('animCreated', this.anim)
     }
+  },
+  mounted () {
+    this.anim = lottie.loadAnimation({
+      container: this.$refs.lavContainer,
+      renderer: 'svg',
+      loop: this.options.loop !== false,
+      autoplay: this.options.autoplay !== false,
+      animationData: this.options.animationData,
+      rendererSettings: this.options.rendererSettings
+      }
+    )
+    this.$emit('animCreated', this.anim)
   }
+}
 </script>
--- src/views/dataIndex/components/tableData.vue
+++ src/views/dataIndex/components/tableData.vue
@@ -1,24 +1,5 @@
 <template>
   <div>
-    <!-- <el-table
-      :data="tableData"
-      class="tableDemo"
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-    </el-table> -->
     <lottie :options="defaultOptions" :height="300" :width="400" v-on:animCreated="handleAnimation"/>
     <div>
         <p>Speed: x{{animationSpeed}}</p>
@@ -33,29 +14,12 @@
 import Lottie from './lottie.vue';
 import * as animationData from "@/assets/loading.json";
 export default {
-  name: 'tableData',
+  name: 'lottieDemo',
   components: {
     'lottie': Lottie
   },
   data() {
     return {
-      // tableData: [{
-      //   date: '2016-05-02',
-      //   name: '王小虎',
-      //   address: '上海市普陀区金沙江路 1518 弄'
-      // }, {
-      //   date: '2016-05-04',
-      //   name: '王小虎',
-      //   address: '上海市普陀区金沙江路 1517 弄'
-      // }, {
-      //   date: '2016-05-01',
-      //   name: '王小虎',
-      //   address: '上海市普陀区金沙江路 1519 弄'
-      // }, {
-      //   date: '2016-05-03',
-      //   name: '王小虎',
-      //   address: '上海市普陀区金沙江路 1516 弄'
-      // }],
       defaultOptions: { animationData: animationData.default },
       animationSpeed: 1
     }
@@ -68,40 +32,15 @@ export default {
     stop: function () {
       this.anim.stop();
     },
-
     play: function () {
       this.anim.play();
     },
-
     pause: function () {
       this.anim.pause();
     },
-
     onSpeedChange: function () {
       this.anim.setSpeed(this.animationSpeed);
     }
   }
 }
 </script>
-<style lang="scss">
-.tableDemo{
-  thead{
-    color: #586c86;
-  }
-  th,tr{
-    background: #1f3853
-  }
-  td,th.is-leaf{
-    border-bottom: 1px solid #182b41;
-  }
-  tbody{
-    color: #ffffff;
-  }
-  .el-table__body tr:hover>td{
-    background-color: #1f3853;
-  }
-}
-.el-table::before{
-  height: 0;
-}
-</style>
